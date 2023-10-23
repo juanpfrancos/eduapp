32fe4ee2590893fa2ad754b91ff40a471a22d812
Juanpfrancos <juanpfrancos@programmer.net>
2023-10-23 16:54:11 -0500

    Add Oracle Connection settings.

--- usersproject/usersproject/settings.py
+++ usersproject/usersproject/settings.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 from decouple import config
 import os
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -64,10 +63,13 @@ WSGI_APPLICATION = "usersproject.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+DATABASES={
+    'default':
+    {
+    'ENGINE':'django.db.backends.oracle',
+    'NAME': config('DB_NAME'),
+    'USER': config('DB_USER'), 
+    'PASSWORD': config('DB_PASSWORD'),
     }
 }
 
