e2519d5ce4480022e3f5075de51e7bc58b5120ef
Juanpfrancos <juanpfrancos@programmer.net>
2023-10-26 09:40:26 -0500

    Fix redirects.

--- usersproject/usersapp/urls.py
+++ usersproject/usersapp/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.register, name='register'),
     path('', views.user_login, name='login'),
+    path('register/', views.register, name='register'),
     path('logout/', views.user_logout, name='logout'),
     path('welcome/', views.user_welcome, name='welcome'),
 ]
