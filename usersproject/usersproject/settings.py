83f9288e0e6f4fc7f04909204e715cd78c8bc9d5
Juanpfrancos <juanpfrancos@programmer.net>
2023-10-26 00:02:19 -0500

    Set static files config.

--- usersproject/usersproject/settings.py
+++ usersproject/usersproject/settings.py
@@ -108,8 +108,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
