a74ba94740ce90cf7f8d8d2e169e32a431b79c89
Juanpfrancos <juanpfrancos@programmer.net>
2023-11-04 00:34:28 -0500

    Rename admin path.

--- usersproject/usersproject/urls.py
+++ usersproject/usersproject/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("adm/", admin.site.urls),
+    path("4dm/", admin.site.urls),
     path("", include("accounts.urls")),
 ]
