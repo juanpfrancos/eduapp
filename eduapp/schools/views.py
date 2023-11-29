c102863fef39bff2dd1d2b040808b394304c4bfe
Juanpfrancos <juanpfrancos@programmer.net>
2023-11-28 20:46:46 -0500

    Set school views with supeuser login.

--- eduapp/schools/views.py
+++ eduapp/schools/views.py
@@ -1,11 +1,16 @@
+from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render, redirect
 from .forms import SchoolForm
 from .models import School
 
+
+@user_passes_test(lambda u: u.is_authenticated and u.role == 'supadmin', login_url='/')
 def school_list(request):
     schools = School.objects.all()
     return render(request, 'school_list.html', {'schools': schools})
 
+
+@user_passes_test(lambda u: u.is_authenticated and u.role == 'supadmin', login_url='/')
 def add_school(request):
     if request.method == 'POST':
         form = SchoolForm(request.POST)
