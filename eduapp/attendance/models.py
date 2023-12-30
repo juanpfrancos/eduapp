2ac8bd24a75ff50a5959cee880aae535edde8672
Juanpfrancos <juanpfrancos@programmer.net>
2023-12-30 14:17:11 -0500

    Add thunter method to show data object for attendance..

--- eduapp/attendance/models.py
+++ eduapp/attendance/models.py
@@ -6,3 +6,6 @@ class Attendance(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     date = models.DateField()
     attended = models.BooleanField(default=False)
+
+    def __str__(self): 
+        return f"{self.student.name} - {self.date}"
